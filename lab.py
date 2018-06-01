2a268fa338cd9be5a286f007ff32b59cefe1bccb
uMtMu <umut1010@gmail.com>
2018-06-01 16:07:14 +0300

    Webargs validation err

--- lab.py
+++ lab.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask.views import MethodView
-from webargs import fields
+from webargs import fields, ValidationError
 from webargs.flaskparser import use_kwargs
 import redis
 r = redis.Redis(host='localhost')
@@ -8,7 +8,12 @@ r = redis.Redis(host='localhost')
 app = Flask(__name__)
 
 class UserAPI(MethodView):
-
+    def name_validator(name):
+        if len(name) > 7:
+            raise ValidationError('Length of user name must shorter than 7', status_code=500)
+        else:
+            return True
+            
     def get(self, user_id):
         if user_id is None:
             return render_template('users.html', objects=[r.get(k) for k in r.keys('user*')])
@@ -16,7 +21,7 @@ class UserAPI(MethodView):
             return render_template('users.html', objects=[r.get('user%s' % user_id)])
     post_args = {
         'id': fields.Int(required=True),
-        'name': fields.Str(required=True, validate=lambda v: len(v) < 7),
+        'name': fields.Str(required=True, validate=name_validator),
         'age': fields.Int(),
     }
     @use_kwargs(post_args, locations=('json', 'form', 'query', 'headers', 'cookies', 'files'))
